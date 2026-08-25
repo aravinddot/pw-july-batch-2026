a5a21c0a09264c694ea3ec4d8976caf963cf603b
Aravind112703 <aravindmathi93@gmail.com>
2026-08-25 21:09:21 +0530

    uploads

--- tests/advanced.spec.ts
+++ tests/advanced.spec.ts
@@ -144,6 +144,124 @@ test.describe('Sandbox Advanced Test cases', ()=> {
 
         await page.waitForTimeout(3000)
 
+    })
+
+
+    test('Handling new tab direct click blocked', async()=> {
+
+        
+        const browser = await chromium.launch()
+
+        const context = await browser.newContext()
+
+        const page = await context.newPage()
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        await page.getByTestId('popup-right-click-link').click()
+
+        await expect(page.getByText('Direct click blocked. Use right click -> Open link in new tab.')).toBeVisible()
+
+
+        const link = await page.getByTestId('popup-right-click-link').getAttribute('href')
+
+        console.log("Link===>"+link)
+
+
+        const pageTwo = await context.newPage()
+
+        await pageTwo.goto(`https://playwright-mastery-academy-app.vercel.app/${link}`)
+
+
+        await expect(pageTwo.getByText('Popup Opened Successfully')).toBeVisible()
+
+
+    })
+
+
+
+    test('Isolated context', async()=> {
+
+        test.setTimeout(180000)
+
+        const browser = await chromium.launch()
+
+        const context = await browser.newContext()
+
+        const page = await context.newPage()
+
+
+        await page.goto('https://testcms.reco-claims.ca/Login')
+
+        await page.locator('[name="Username"]').fill('info+programmanager@xlgclaims.com')
+
+        await page.locator('[name="Password"]').fill('XlgTest-2026!')
+
+        await page.locator('[type="submit"]').click()
+
+        await page.waitForTimeout(15000)
+
+//---------------------------------------------------------------------------------
+
+         const contextTwo = await browser.newContext()
+
+        const pageTwo = await contextTwo.newPage()
+
+
+        await pageTwo.goto('https://testcms.reco-claims.ca/Login')
+
+        await pageTwo.locator('[name="Username"]').fill('info+programmanager@xlgclaims.com')
+
+        await pageTwo.locator('[name="Password"]').fill('XlgTest-2026!')
+
+        await pageTwo.locator('[type="submit"]').click()
+
+        await pageTwo.waitForTimeout(15000)
+
+
+        const cookie = await context.cookies()
+
+        const cookieTwo = await contextTwo.cookies()
+
+        console.log("Cookie One===>"+JSON.stringify(cookie))
+
+        console.log("Cookie Two===>"+JSON.stringify(cookieTwo))
+
+
+    })
+
+
+    test('Handling drag and drop', async({page})=> {
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        await page.getByTestId('drag-source').dragTo(page.getByTestId('drop-target'))
+
+        await expect(page.getByText('Drop completed successfully.')).toBeVisible()
+
+
+    })
+
+
+    test('Single and multiple files upload', async({page})=> {
+
+        await page.goto('https://playwright-mastery-academy-app.vercel.app/practice/sandbox-advanced')
+
+        await page.getByTestId('file-upload-input').setInputFiles('uploads/practice-report.pdf')
+
+        await expect(page.getByText('practice-report.pdf uploaded successfully.')).toBeVisible()
+
+
+
+        await page.getByTestId('multi-file-upload-input').setInputFiles([
+            'uploads/practice-data.csv',
+            'uploads/practice-data.xml',
+            'uploads/practice-notes.txt',
+            'uploads/practice-report.pdf'
+        ])
+
+        await expect(page.getByText('4 files uploaded')).toBeVisible()
+
 
 
 
